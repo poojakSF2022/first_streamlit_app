845dfd1f89d2d06982b6180405749f76ec21a0b5
poojakSF2022 <113716359+poojakSF2022@users.noreply.github.com>
2022-09-16 13:59:29 -0400

    Update streamlit_app.py
    added pandas libraries, added csv file read to add table of data

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ streamlit.header("Anvi's Kitchen")
 streamlit.title("My first streamlit app")
 streamlit.title("Rich Chocolate coffee Cake")
 streamlit.title("Pomogranade Juice")
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
