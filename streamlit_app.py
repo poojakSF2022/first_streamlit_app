6ba0560c73a445eb277712a8cc381a28177b55e7
poojakSF2022 <113716359+poojakSF2022@users.noreply.github.com>
2022-09-16 16:26:43 -0400

    Update streamlit_app.py
    anvi's changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,9 @@ streamlit.header("Anvi's Kitchen")
 streamlit.title("My first streamlit app")
 streamlit.title("Rich Chocolate coffee Cake")
 streamlit.title("Pomogranade Juice")
+streamlit.title("Chocolate Bownie")
+streamlit.title("CheakPee Salad")
+
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
